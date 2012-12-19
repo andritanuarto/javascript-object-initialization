5210c8d9dddcdbced4f205085d3208b938e2b4b6
Andri Tanuarto <andritanuarto@gmail.com>
2012-12-19 00:16:35 -0800

    Create object_initialization.js

---
+++ object_initialization.js
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Object Initialization</title>
+</head>
+
+<body>
+<script type="text/javascript">
+//computer is object drive is property with value is floppy
+computer={drive:"floppy", cpu:"intel", ram:"ddr"}
+
+document.write("I want a " + computer.drive + " and " + computer.cpu + " and " + computer.ram);
+</script>
+</body>
+</html>
